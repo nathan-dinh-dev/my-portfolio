2b2e30a9e5584792c9b0dc1376d5d2abe6228da3
Nathan Dinh <nathanxdinhx97@gmail.com>
2023-10-24 01:02:25 -0700

    Added navigation bar

---
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -0,0 +1,94 @@
+import styles from "./Header.module.css";
+import {
+  TOTAL_SCREEENS,
+  GET_SCREEN_INDEX,
+} from "../../../utilities/commonUtils";
+import ScrollService from "../../../utilities/ScrollService";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
+
+const Header = () => {
+  const [selectedScreen, setSelectedScreen] = useState(0);
+  const [showHeaderOptions, setShowHeaderOptions] = useState(false);
+
+  const updateCurrentScreen = (currentScreen) => {
+    if (!currentScreen || !currentScreen.screenInView) return;
+
+    let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView);
+    if (screenIndex < 0) return;
+  };
+
+  let currentScreenSubscription =
+    ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
+
+  const getHeaderOptions = () => {
+    return TOTAL_SCREEENS.map((screen, i) => (
+      <div
+        key={screen.screen_name}
+        className={getHeaderOptionsClasses(i)}
+        onClick={() => switchScreen(i, screen)}
+      >
+        <span>{screen.screen_name}</span>
+      </div>
+    ));
+  };
+
+  const getHeaderOptionsClasses = (index) => {
+    let classes = styles["header-option"];
+    if (index < TOTAL_SCREEENS.length - 1)
+      classes += styles["header-option-seperator"];
+
+    if (selectedScreen === index) classes += styles["selected-header-option"];
+
+    return classes;
+  };
+
+  const switchScreen = (index, screen) => {
+    let screenComponent = document.getElementById(screen.screen_name);
+    if (!screenComponent) return;
+    screenComponent.scrollIntoView({ behavior: "smooth" });
+    setSelectedScreen(index);
+    setShowHeaderOptions(false);
+  };
+
+  useEffect(() => {
+    return () => {
+      currentScreenSubscription.unsubscribe();
+    };
+  }, [currentScreenSubscription]);
+
+  return (
+    <div
+      className={styles["header-container"]}
+      onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+    >
+      <div className={styles["header-parent"]}>
+        <div
+          className={styles["header-hamburger"]}
+          onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+        >
+          <FontAwesomeIcon
+            className={`header-hamburger-bars ${styles["header-hamburger-bars"]}`}
+            icon={faBars}
+          />
+        </div>
+
+        <div className={styles["header-logo"]}>
+          <span>Nathan~</span>
+        </div>
+        <div
+          className={
+            showHeaderOptions
+              ? `${styles["header-options"]} ${styles["show-hamburger-options"]}`
+              : styles["header-options"]
+          }
+        >
+          {getHeaderOptions()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/PortfolioContainer/Home/Home.js
+++ src/PortfolioContainer/Home/Home.js
@@ -1,10 +1,12 @@
 import styles from "./Home.module.css";
 import Profile from "./Profile/Profile";
 import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
 
 const Home = () => {
   return (
     <div className={styles["home-container"]}>
+      <Header />
       <Profile />
       <Footer />
     </div>
